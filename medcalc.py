5324d16c1da270affcdc976ea9b3db45e3d7fd8f
Chris <chris.baumgartner115@gmail.com>
2023-08-18 19:19:38 +0200

    modified and features added

--- medcalc.py
+++ medcalc.py
@@ -1,31 +1,47 @@
 import math
 import sys
 
-def decay_function(n_0, half_life, t):
+def decay_function(n_0, half_life, h):
     decay_constant = math.log(2) / half_life
-    return n_0 * math.pow(math.e, - decay_constant * t)
+    return n_0 * math.pow(math.e, - decay_constant * h)
 
 def medication_level_calc(dose, times_per_day, half_life, duration):
     med_level = 0
-    meds_taken_hours = 24 // times_per_day
-    for i in range(0, duration * 24):
-        med_level = decay_function(med_level, half_life, i)
-        if i % meds_taken_hours:
+    intermission_hours = 24 // times_per_day
+    cumulative_hours = duration * 24
+    for i in range(0, cumulative_hours):
+        med_level = decay_function(med_level, half_life, 1)
+        if i % intermission_hours == 0:
             med_level += dose
+        print("%dd %dh: %.2f" % (i // 24, i % 24, med_level))
+
+def decay_visualization(n_0, half_life):
+    n = n_0
+    hours = 0
+    while (n / n_0 * 100) >= 2.5:
+        n = decay_function(n_0, half_life, hours)
+        print("%dd: %.2f%% %.2f" % (hours // 24, n / n_0 * 100, n ))
+        hours += 24
+
 
 
 num_sys_args = len(sys.argv)
 if sys.argv[1] == "-df" and num_sys_args == 5:
-    n_0 = sys.argv[2]
-    half_life = sys.argv[3]
-    t = sys.argv[4]
-    print(decay_function(n_0, half_life, t))
+    n_0 = int(sys.argv[2])
+    half_life = int(sys.argv[3])
+    h = int(sys.argv[4])
+    print("%.2f" % decay_function(n_0, half_life, h))
 elif sys.argv[1] == "-mlc" and num_sys_args == 6:
-    dose = sys.argv[2]
-    times_per_day = sys.argv[3]
-    half_life = sys.argv[4]
-    duration = sys.argv[5]
+    dose = int(sys.argv[2])
+    times_per_day = int(sys.argv[3])
+    half_life = int(sys.argv[4])
+    duration = int(sys.argv[5])
+    medication_level_calc(dose, times_per_day, half_life, duration)
+elif sys.argv[1] == "-dv" and num_sys_args == 4:
+    n_0 = int(sys.argv[2])
+    half_life = int(sys.argv[3])
+    decay_visualization(n_0, half_life)
 else:
-    print("""USAGE: %s -df n_0 half_life t
-        OR: %s - n_0 half_life t""" % (sys.argv[0]))
+    print("""USAGE: %s -df n_0 half_life h
+        OR: %s -mlc dose times_per_day half_life duration""" % (sys.argv[0]))
     
